957ad0d0929806330150e46ef72e61eada7cd1a3
leandroquiroga <leandroquiroga9514@gmail.com>
2023-02-11 16:44:19 -0300

    fix: Has been changed the name of router

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 
-import routes from '../routes';
 import { dbConnect } from '../database';
+import router from '../router';
 
 const server = express();
 dbConnect();
 server.use(cors());
 server.use(express.json());
-server.use('/v1/api', routes);
+server.use('/v1/api', router);
 
 export default server;
