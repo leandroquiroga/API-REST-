23f3e82de7c1226fccc2322ed82c440eea9953d6
leandroquiroga <leandroquiroga9514@gmail.com>
2023-02-11 16:40:04 -0300

    fix: Has been changed the type id - before: number after: string

--- dao/index.ts
+++ dao/index.ts
@@ -5,8 +5,8 @@ export const usersDAO = {
   save: (payload: User) => userModel.create(payload),
   saveMany: (payload: User[]) => userModel.insertMany(payload),
   getAll: () => userModel.find(),
-  getOne: (id: number) => userModel.findById({ id }),
-  updateOne: (id: number, payload: User) => userModel.findByIdAndUpdate({ id }, payload),
-  deleteOne: (id: number) => userModel.findByIdAndDelete({ id }),
+  getOne: (id: string) => userModel.findById(id),
+  updateOne: (id: string, payload: User) => userModel.findByIdAndUpdate( id , payload),
+  deleteOne: (id: string) => userModel.findByIdAndDelete( id ),
   deleteAll: () => userModel.collection.drop(),
 };
