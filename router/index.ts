6be23703c8901ed4b6a85989249944da78cc774a
leandroquiroga <leandroquiroga9514@gmail.com>
2023-02-18 19:17:43 -0300

    feat: Has been added two router put and delete

--- router/index.ts
+++ router/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { getUser, postUsers } from '../controllers';
+import { getUser, postUsers, updateUser } from '../controllers';
 import { validator } from '../utilities';
-import { validationField } from '../middlewares/index';
+import { deleteUser } from '../controllers/index';
 
 const router = Router();
 
 router.get('/users', getUser);
-router.get('/users/:id', validator.isValidId, validationField ,getUser);
-router.post('/users', validator.createUser, validationField, postUsers);
-
+router.get('/users/:id', validator.isValidId ,getUser);
+router.post('/users', validator.createUser, postUsers);
+router.put('/users/:id', validator.isValidId, validator.updateValidator, updateUser)
+router.delete('/users/:id', validator.isValidId, deleteUser)
 export default router;
