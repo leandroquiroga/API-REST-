a2f13fa3f2417256f57e87f17b22bae75bed7955
leandroquiroga <leandroquiroga9514@gmail.com>
2023-02-18 19:18:15 -0300

    feat: Has been added news validations

--- utilities/index.ts
+++ utilities/index.ts
@@ -1,6 +1,13 @@
 import { check } from 'express-validator';
 import pino from 'pino';
-import { isValidDomian, isValidID } from '../helpers';
+import {
+  isEmailAlready,
+  isUsernameAlready,
+  isValidDomian,
+  isValidID,
+  isValidRange,
+} from '../helpers';
+import { validationField } from '../middlewares';
 
 export const logger = pino({
   transport: {
@@ -12,22 +19,65 @@ export const logger = pino({
 });
 
 export const validator = {
-  isValidId: check('id')
-    .notEmpty()
-    .withMessage('ID es requerido.')
-    .bail()
-    .isMongoId()
-    .withMessage('Debe ser un ID valido para mongo')
-    .bail()
-    .custom(isValidID),
+  isValidId: [
+    check('id')
+      .notEmpty()
+      .withMessage('ID es requerido.')
+      .bail()
+      .isMongoId()
+      .withMessage('Debe ser un ID valido para mongo')
+      .bail()
+      .custom(isValidID),
+    validationField,
+  ],
   createUser: [
     check('email')
       .notEmpty()
-      .withMessage('Email es requerido')
+      .withMessage('Email es obligatorio')
       .bail()
       .isEmail()
       .withMessage('Debe ser un email valido')
       .bail()
       .custom(isValidDomian)
+      .bail()
+      .custom(isEmailAlready),
+    validationField,
+    check('age')
+      .notEmpty()
+      .withMessage('La edad es obligatorio')
+      .bail()
+      .isNumeric()
+      .withMessage('La edad debe ser un valor numerico')
+      .bail()
+      .custom(isValidRange),
+    validationField,
+    check('name')
+      .notEmpty()
+      .withMessage('El nombre es obligatorio')
+      .bail()
+      .isLength({ min: 3, max: 20 })
+      .withMessage('El nombre detener entre 3 y 20 caracteres')
+      .bail(),
+    validationField,
+    check('username')
+      .notEmpty()
+      .withMessage('El username es obligatorio')
+      .bail()
+      .custom(isUsernameAlready),
+    validationField,
+    check('lastname')
+      .notEmpty()
+      .withMessage('El apellido es obligatorio')
+      .bail()
+      .isLength({ min: 3, max: 20 })
+      .withMessage('El nombre detener entre 3 y 20 caracteres')
+      .bail(),
+    validationField,
+  ],
+  updateValidator: [
+    check('email').custom(isEmailAlready),
+    validationField,
+    check('username').custom(isUsernameAlready),
+    validationField,
   ],
 };
