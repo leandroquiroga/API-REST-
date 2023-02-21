78f62293aaec8f8551732ef0f0a5cecd76047643
leandroquiroga <leandroquiroga9514@gmail.com>
2023-02-21 17:47:50 -0300

    Has been added mock payload

---
+++ mock/payload.mock.ts
@@ -0,0 +1,9 @@
+import { User } from '../interfaces/index';
+
+export const payload: User = {
+  name: "Emanuel",
+  lastname: "Rodriguez",
+  username: "emaquiro2",
+  age: 39,
+  email: "ema3@gmail.com"
+}
