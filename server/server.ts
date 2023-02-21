5a9c83353fe5de629a1053d38b2d8cb409a7276d
leandroquiroga <leandroquiroga9514@gmail.com>
2023-02-21 17:49:58 -0300

    feat: Has been added middelwares errorServer and errorHandler

--- server/server.ts
+++ server/server.ts
@@ -3,11 +3,15 @@ import cors from 'cors';
 
 import { dbConnect } from '../database';
 import router from '../router';
+import {errorHandler, errorServer} from '../utilities/errors/error'
 
 const server = express();
 dbConnect();
 server.use(cors());
 server.use(express.json());
 server.use('/v1/api', router);
+
+server.use(errorHandler);
+server.use(errorServer);
 
 export default server;
