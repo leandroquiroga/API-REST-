84f8642904e203402e02928e0a1eca30b963a7c4
leandroquiroga <leandroquiroga9514@gmail.com>
2023-02-21 17:42:43 -0300

    fix: Has benn modified method getAll

--- dao/index.ts
+++ dao/index.ts
@@ -4,7 +4,12 @@ import userModel from "../models/user.model";
 export const usersDAO = {
   save: (payload: User) => userModel.create(payload),
   saveMany: (payload: User[]) => userModel.insertMany(payload),
-  getAll: () => userModel.find(),
+  getAll: (limit?: number, from?: number) => {
+    if(limit && from) 
+      return userModel.find().skip(from).limit(limit);
+    
+    return userModel.find();
+  },
   getOneByID: (id: string) => userModel.findById(id),
   getOneEmail: (email: string) => userModel.findOne({ email }),
   getOneUsername: (username: string) => userModel.findOne({ username }),
